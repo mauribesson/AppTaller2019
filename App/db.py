f41ba0b403c2f3adca2d0d7c52a5c7121b67e163
mauribesson <mauribesson@gmail.com>
2019-10-10 00:55:25 -0300

    inicio de armado de db.py

---
+++ App/db.py
@@ -0,0 +1,16 @@
+import psycopg2
+
+class Database: 
+    def __init__(self):
+        pass
+
+    def query(self, query=""):  
+        self.connect = psycopg2.connect(host="localhost", port="5432", database="PythonWebApp", user="postgres", password="12345678")
+        self.cursor = self.connect.cursor()    
+        self.cursor.execute(query)
+        #result = self.cursor.fetchone()
+        result = self.cursor.fetchall()
+        #self.cursor.close()
+        self.cursor.close()
+        self.connect.close()
+        return result
