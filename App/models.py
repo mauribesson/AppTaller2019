099c2a4ce9328adb77b2ae901267579b0b37fb7a
mauribesson <mauribesson@gmail.com>
2019-10-16 20:16:04 -0300

    se agrega imagen de prueba a img

---
+++ App/models.py
@@ -0,0 +1,23 @@
+
+#Clase Usuario 
+class Usuario:
+    def __init__(self):
+        self.nombre = ""
+        self.contrasenia = ""
+        self.contacto = ""
+
+    def alta_usruario(self):
+        pass
+
+    def baja_usuario(self):
+        pass
+
+    def modificar_usuario(self):
+        pass
+
+    def consultar_usuario(self):
+        pass
+
+    
+
+
